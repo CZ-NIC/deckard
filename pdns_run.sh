ec4594a9fc6de2cb60b05fc0c70fe468c286f5fb
Pieter Lexis <pieter.lexis@powerdns.com>
2016-04-28 19:23:31 +0200

    Enable/Allow PowerDNS Recursor DNSSEC tests

--- pdns_run.sh
+++ pdns_run.sh
@@ -4,13 +4,13 @@
 TESTS=sets/resolver
 
 # Path to daemon
-DAEMON=pdns_recursor
+DAEMON=$HOME/src/PowerDNS/pdns/pdns/recursordist/pdns_recursor
      
 # Template file name
-TEMPLATE=template/recursor.j2:template/hints_pdns.j2
+TEMPLATE=template/recursor.j2:template/hints_pdns.j2:template/dnssec_pdns.j2
 
 # Config file name
-CONFIG=recursor.conf:hints.pdns
+CONFIG=recursor.conf:hints.pdns:dnssec.lua
 
 # Additional parameter for pdns_recursor
 # it means configuration file can be found in working directory
