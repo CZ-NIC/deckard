b6eaecf3df8e28f3abf703d9c0d1c924c3d9fc5f
Filip Široký <filip.siroky@nic.cz>
2018-04-05 09:06:46 +0200

    Change used image

--- ci/runlocally-docker.sh
+++ ci/runlocally-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ''' A script for running Deckard tests in docker locally. '''
+IMAGE="registry.labs.nic.cz/knot/knot-resolver/ci:debian-stable"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DECKARD=$(dirname $DIR)
 MOUNT=''
@@ -22,17 +23,7 @@ do
 	fi
 done
 
-echo "Start building Docker image"
-docker build -t deckard/ci:local $DIR
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-	echo Failed to build docker image.
-	exit $STATUS
-else
-	echo "Finished building Docker image"
-fi
-
-ID=$(docker run -d -v "$DECKARD:$DECKARD" $MOUNT -it deckard/ci:local)
+ID=$(docker run -d -v "$DECKARD:$DECKARD" $MOUNT -it $IMAGE)
 docker exec -it $ID bash -c "cd $DECKARD && make depend && $*"
 docker stop $ID &>/dev/null
 docker rm $ID &>/dev/null
