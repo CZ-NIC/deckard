1444c492a6cf970e6a2941c3ac9cd8b8710f03a8
Petr Špaček <petr.spacek@nic.cz>
2018-04-05 18:06:37 +0200

    run.sh: add support for DECKARDFLAGS environment variable
    It is Deckard-specific equivalent of MAKEFLAGS, which is very handy for
    default flags like e.g. parallel execution.
    
    Use DECKARDFLAGS="-n $(nproc)" to run as many tasks as you have cores.

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ source "${MAKEDIR}/env.sh"
 
 # compatibility with old TESTS= env variable
 # add --scenarios= only if the variable TESTS is non-empty
-python3 -m pytest -q tests/test_runner.py ${TESTS:+"--scenarios=${TESTS}"} "$@"
+python3 -m pytest -q tests/test_runner.py ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} "$@"
