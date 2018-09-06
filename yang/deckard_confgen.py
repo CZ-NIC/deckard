208655c13eb12c9dca1ef6cefec5e535825e1743
Ales Mrazek <ales.mrazek@nic.cz>
2018-09-06 13:31:19 +0200

    update of yang model and deckard_confgen.py

---
+++ yang/deckard_confgen.py
@@ -0,0 +1,47 @@
+import sys
+from yangson.datamodel import DataModel
+from json import load
+from resolvers_yang.generator import Generator
+
+json_path = sys.argv[1]
+
+# set file names and paths
+yangdir = "yang-modules"
+kresd_path = "kresd.conf"
+unb_path = "unbound.rpl"
+
+# load model
+model = DataModel.from_file(yangdir + "/yanglib-deckard.json", [yangdir])
+
+# load data from json
+with open(json_path) as infile:
+    ri = load(infile)
+
+data = model.from_raw(ri)
+
+# validate against data model
+data.validate()
+
+# get path where is mock data located
+mock_path = data["cznic-deckard:deckard"]["mock-data"].value
+
+# load mock data text file from path as string
+mock_data = open(mock_path).read()
+
+# slicing mock_data
+mock_begin = mock_data.find("SCENARIO_BEGIN")
+mock_data = mock_data[mock_begin:]
+
+# generate configuration strings
+unb_conf = Generator.unbound(data)
+kresd_conf = Generator.kresd(data)
+
+# write kresd.conf
+knot_file = open(kresd_path, "w+")
+knot_file.write(kresd_conf)
+knot_file.close()
+
+# write unbound.rpl = unbconf string + mock data
+unb_file = open(unb_path, "w+")
+unb_file.write(unb_conf + "\nCONFIG_END\n\n" + mock_data)
+unb_file.close()
