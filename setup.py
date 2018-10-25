45aa6eb7284db66edb9e73d7a57691a2d634082b
Tomas Krizek <tomas.krizek@nic.cz>
2018-10-25 13:26:03 +0200

    mypy: modify setup to avoid param issues

--- setup.py
+++ setup.py
@@ -3,27 +3,27 @@ from distutils.core import setup
 
 version = '3.0'
 
-kwargs = {
-    'name': 'deckard',
-    'version': version,
-    'description': 'DNS toolkit',
-    'long_description':
-    """Deckard is a DNS software testing based on library pydnstest.
-       It supports parsing and running Unbound-like test scenarios,
-       and setting up a mock DNS server. It's based on dnspython.""",
-    'author': 'CZ.NIC',
-    'author_email': 'knot-dns-users@lists.nic.cz',
-    'license': 'BSD',
-    'url': 'https://gitlab.labs.nic.cz/knot/deckard',
-    'packages': ['pydnstest'],
-    'python_requires': '>=3.5',
-    'install_requires': [
+setup(
+    name='deckard',
+    version=version,
+    description='DNS toolkit',
+    long_description=(
+       "Deckard is a DNS software testing based on library pydnstest."
+       "It supports parsing and running Unbound-like test scenarios,"
+       "and setting up a mock DNS server. It's based on dnspython."),
+    author='CZ.NIC',
+    author_email='knot-dns-users@lists.nic.cz',
+    license='BSD',
+    url='https://gitlab.labs.nic.cz/knot/deckard',
+    packages=['pydnstest'],
+    python_requires='>=3.5',
+    install_requires=[
         'dnspython>=1.15',
         'jinja2',
         'PyYAML',
         'python-augeas'
     ],
-    'classifiers': [
+    classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3 :: Only'
@@ -32,7 +32,5 @@ kwargs = {
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
-    ],
-}
-
-setup(**kwargs)
+    ]
+)
