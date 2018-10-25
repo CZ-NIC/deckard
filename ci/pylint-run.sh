1a8cc04c046ed6febb46e45e6a26b3fd1a9baaa1
Tomas Krizek <tomas.krizek@nic.cz>
2018-10-25 10:17:40 +0200

    pylint: use pylint properly with pylintrc

---
+++ ci/pylint-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o nounset -o xtrace -o errexit
+source "$(dirname "$0")/common.sh"
+
+PYFILES=$(find . \
+	-path ./.git -prune -o \
+	-path ./contrib -o \
+	-type d -exec test -e '{}/__init__.py' \; -print -prune -o \
+	-name '*.py' -print -o \
+	-type f -exec grep -qsm1 '^#!.*\bpython' '{}' \; -print)
+
+${PYTHON} -m pylint -j 0 --rcfile pylintrc ${PYFILES}
