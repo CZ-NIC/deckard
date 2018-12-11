dc77b0b0c99158932b0f980fa34c2b8863efcb7a
Štěpán Balážik <tpn Balik>
2018-12-11 10:41:52 +0100

    wip

--- ci/compare-rplint.sh
+++ ci/compare-rplint.sh
@@ -6,7 +6,7 @@ source "$(dirname "$(readlink -f "$0")")/common.sh"
 function find_new_tests {
 	: detect tests affected by current merge request
 	: store list of modified tests in ${NEW_TESTS_FILE}
-  git diff --name-only --diff-filter=A ${MERGEBASE} ${HEAD} | fgrep .rpl > "${NEW_TESTS_FILE}" || : no new tests detected
+  git diff --name-only --diff-filter=ACMR ${MERGEBASE} ${HEAD} | fgrep .rpl > "${NEW_TESTS_FILE}" || : no new tests detected
 }
 
 NEW_TESTS_FILE="/tmp/new_tests"
