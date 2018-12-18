9c3ec7b86725e6b9d2faef334d9395c2b4fae0c0
Štěpán Balážik <tpn Balik>
2018-12-18 13:55:54 +0100

    tools: network and forwarder checks
    Inspired by RFC8027 we implement simple checks that diagnose the sanity of network enviroment (or a forwarding resolver) for use of DNSSEC.

---
+++ tools/conftest.py
@@ -0,0 +1,11 @@
+import ipaddress
+
+
+def pytest_addoption(parser):
+    parser.addoption("--forwarder", action="store", help="IP of forwarder to test")
+
+
+def pytest_generate_tests(metafunc):
+    if 'forwarder' in metafunc.fixturenames:
+        forwarder = metafunc.config.option.forwarder
+        metafunc.parametrize("forwarder", [ipaddress.ip_address(forwarder)], ids=str)
