1761bd76571967455d45741ab3e1f58d433592f8
Štěpán Balážik <tpn Balik>
2018-12-19 14:18:06 +0100

    check that all the addresses binary asks are present in scenario
    This check is false positive on RPLs containing REPLY steps.
    Therefore we check for them and warn the user as well.

--- deckard.py
+++ deckard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from datetime import datetime
 import errno
+import ipaddress
 import logging
 import logging.config
 import os
@@ -9,6 +10,7 @@ import socket
 import subprocess
 import tempfile
 import time
+from typing import Set  # noqa
 
 import dpkt
 import jinja2
@@ -346,6 +348,39 @@ def check_for_icmp():
         return False
 
 
+def check_for_reply_steps(case: scenario.Scenario) -> bool:
+    return any(s.type == "REPLY" for s in case.steps)
+
+
+def check_for_unknown_servers(case: scenario.Scenario, daemon: dict) -> None:
+    """ Checks Deckards's PCAP for packets going to servers not present in scenario """
+    path = os.path.join(daemon["cfg"]["dir"], "pcap")
+    asked_servers = set()
+    with open(path, "rb") as f:
+        pcap = dpkt.pcap.Reader(f)
+        for _, packet in pcap:
+            try:
+                ip = dpkt.ip.IP(packet)
+            except dpkt.dpkt.UnpackError:
+                ip = dpkt.ip6.IP6(packet)
+            # pylint: disable=no-member
+            dest = ipaddress.ip_address(int.from_bytes(ip.dst, byteorder="big"))
+            # pylint: enable=no-member
+            if dest.is_global:
+                asked_servers.add(dest)
+
+    scenario_ips = set()  # type: Set[str]
+    for r in case.ranges:
+        scenario_ips |= r.addresses
+
+    scenario_servers = {ipaddress.ip_address(ip) for ip in scenario_ips}
+
+    if not scenario_servers >= asked_servers:
+        if not check_for_reply_steps(case):
+            raise RuntimeError("Binary in test asked an IP address not present in scenario %s"
+                               % (asked_servers - scenario_servers))
+
+
 def run_testcase(daemons, case, root_addr, addr_family, prog_under_test_ip):
     """Run actual test and raise exception if the test failed"""
     server = testserver.TestServer(case, root_addr, addr_family)
@@ -366,13 +401,19 @@ def run_testcase(daemons, case, root_addr, addr_family, prog_under_test_ip):
             if daemon['proc'].returncode != 0 and not ignore_exit:
                 raise ValueError('process %s terminated with return code %s'
                                  % (daemon['cfg']['name'], daemon['proc'].returncode))
+            check_for_unknown_servers(case, daemon)
+
+    if check_for_reply_steps(case):
+        logging.warning("%s has REPLY steps in it. These are known to fail randomly. "
+                        "Errors might be false positives.", case.file)
+
     # Do not clear files if the server crashed (for analysis)
     if server.undefined_answers > 0:
         # Deckard's responses to resolvers might be delayed due to load which
         # leads the resolver to close the port and to the test failing in the
         # end. We partially detect these by checking the PCAP for ICMP packets.
         if check_for_icmp():
-            logging.error("Deckard is under load.\
-Other errors might be false negatives.\
-Consider retrying the job later.")
+            logging.error("Deckard is under load. "
+                          "Other errors might be false negatives. "
+                          "Consider retrying the job later.")
         raise ValueError('the scenario does not define all necessary answers (see error log)')
