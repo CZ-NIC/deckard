d54e7784c1b769fd9a41b9da0dce3e05cee8fd31
Tomas Krizek <tomas.krizek@nic.cz>
2019-01-29 11:50:18 +0100

    ci/mypy: run checks separately for tools

--- ci/mypy-run.sh
+++ ci/mypy-run.sh
@@ -3,11 +3,15 @@ set -o nounset -o errexit
 source "$(dirname "$0")/common.sh"
 
 PYFILES=$(find . \
-	-path ./.git -prune -o \
-	-path ./contrib -o \
-	-type d -exec test -e '{}/__init__.py' \; -print -prune -o \
-	-name '*.py' -print -o \
-	-type f -exec grep -qsm1 '^#!.*\bpython' '{}' \; -print)
+    -path ./.git -prune -o \
+    -path ./contrib -o \
+    -path ./tools -prune -o \
+    -type d -exec test -e '{}/__init__.py' \; -print -prune -o \
+    -name '*.py' -print -o \
+    -type f -exec grep -qsm1 '^#!.*\bpython' '{}' \; -print)
 set -e
 
 ${PYTHON} -m mypy --ignore-missing-imports ${PYFILES}
+
+# tools
+${PYTHON} -m mypy --ignore-missing-imports tools
