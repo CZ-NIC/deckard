2753e17da285ee623cf8d40cd74f6696691141af
Štěpán Balážik <tpn Balik>
2019-02-25 19:19:20 +0100

    pydnstest: make "ADJUST raw_id" work again
    Fixes #40.

---
+++ ci/raw_id.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+import dns.message
+
+from pydnstest import mock_client
+
+q = dns.message.make_query("anything", "A")
+qid = q.id.to_bytes(2, "big", signed=False)
+sock = mock_client.setup_socket("1.2.3.4", 53)
+mock_client.send_query(sock, q)
+a = mock_client.get_answer(sock)
+print(qid)
+print(a)
+
+
+if a == qid:
+    sys.exit(0)
+else:
+    sys.exit(1)
