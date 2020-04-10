5fc3b07562d35ed37f4db71af68ae8858fa1eb39
Štěpán Balážik <stepan.balazik@nic.cz>
2020-04-10 11:34:03 +0200

    replace socket wrapper with Linux namespace

--- deckard_pytest.py
+++ deckard_pytest.py
@@ -1,13 +1,19 @@
 import logging
 import os
-import subprocess
 import random
+import shutil
+import subprocess
 import sys
+import tempfile
 import time
+from ipaddress import ip_address
 
+import dpkt
 import pytest
 
 import deckard
+from namespaces import LinuxNamespace
+from networking import InterfaceManager
 
 
 def set_coverage_env(path, qmin):
@@ -35,14 +41,104 @@ logging.getLogger("augeas").setLevel(logging.ERROR)
 check_platform()
 
 
+class DeckardUnderLoadError(Exception):
+    pass
+
+
+class TCPDump:
+    """This context manager captures a PCAP file and than checks it for obvious errors."""
+
+    # -f option filters out all ICMP messages that are not Destination Unreachable
+    DUMPCAP_CMD = ["dumpcap", "-i", "any", "-q", "-f",
+                   "not icmp6[0]==135 and not icmp6[0]==133 and not (ip6[6]==0 and ip6[40]==58)",
+                   "-P", "-w"]
+
+    def __init__(self, config):
+        self.config = config
+        self.config["tmpdir"] = self.get_tmpdir()
+        self.tcpdump = None
+        self.config["pcap"] = os.path.join(self.config["tmpdir"], "deckard.pcap")
+
+    def __enter__(self):
+        cmd = self.DUMPCAP_CMD.copy()
+        cmd.append(self.config["pcap"])
+        self.tcpdump = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    def __exit__(self, _, exc_value, __):
+        if exc_value is not None or self.config.get('noclean'):
+            # Wait for the PCAP to be finalized
+            time.sleep(1)
+
+        self.tcpdump.terminate()
+
+        self.check_for_unknown_server()
+
+        if exc_value is None:
+            if self.config.get('noclean'):
+                # Do not clear files if the server crashed (for analysis)
+                logging.getLogger('deckard.hint').info(
+                    'test working directory %s', self.config["tmpdir"])
+            else:
+                shutil.rmtree(self.config["tmpdir"])
+        else:
+            if isinstance(exc_value, ValueError):
+                self.check_for_icmp()
+            raise
+
+    @staticmethod
+    def get_tmpdir():
+        if "DECKARD_DIR" in os.environ:
+            tmpdir = os.environ["DECKARD_DIR"]
+            if os.path.lexists(tmpdir):
+                raise ValueError('DECKARD_DIR "%s" must not exist' % tmpdir)
+        else:
+            tmpdir = tempfile.mkdtemp(suffix='', prefix='tmpdeckard')
+
+        return tmpdir
+
+    def check_for_icmp(self):
+        """ Checks Deckards's PCAP for ICMP packets """
+        # Deckard's responses to resolvers might be delayed due to load which
+        # leads the resolver to close the port and to the test failing in the
+        # end. We partially detect these by checking the PCAP for ICMP packets.
+        udp_seen = False
+        with open(self.config["pcap"], "rb") as f:
+            pcap = dpkt.pcap.Reader(f)
+            for _, packet in pcap:
+                ip = dpkt.sll.SLL(packet).data
+
+                if isinstance(ip.data, dpkt.udp.UDP):
+                    udp_seen = True
+
+                if udp_seen:
+                    if isinstance(ip.data, (dpkt.icmp.ICMP, dpkt.icmp6.ICMP6)):
+                        raise DeckardUnderLoadError("Deckard is under load. "
+                                                    "Other errors might be false negatives. "
+                                                    "Consider retrying the job later.")
+
+    def check_for_unknown_server(self):
+        unknown_addresses = set()
+        with open(self.config["pcap"], "rb") as f:
+            pcap = dpkt.pcap.Reader(f)
+            for _, packet in pcap:
+                ip = dpkt.sll.SLL(packet).data
+                dest = str(ip_address(ip.dst))
+                if dest not in self.config["if_manager"].added_addresses:
+                    unknown_addresses.add(dest)
+
+        if unknown_addresses:
+            raise RuntimeError("Binary under test queried an IP address not present"
+                               " in scenario %s" % unknown_addresses)
+
+
 def run_test(path, qmin, config, max_retries, retries=0):
     set_coverage_env(path, qmin)
+
     try:
-        del os.environ["SOCKET_WRAPPER_DIR"]
-    except KeyError:
-        pass
-    try:
-        deckard.process_file(path, qmin, config)
+        with LinuxNamespace("net"):
+            config["if_manager"] = InterfaceManager()
+            with TCPDump(config):
+                deckard.process_file(path, qmin, config)
     except deckard.DeckardUnderLoadError as e:
         if retries < max_retries:
             logging.error("Deckard under load. Retrying…")
