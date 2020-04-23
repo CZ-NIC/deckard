99516155da655a69e3274ed0cbc1bf6462f7ca49
Štěpán Balážik <stepan.balazik@nic.cz>
2020-04-23 16:52:25 +0200

    WIP

--- deckard_pytest.py
+++ deckard_pytest.py
@@ -59,7 +59,8 @@ class TCPDump:
     def __enter__(self):
         cmd = self.DUMPCAP_CMD.copy()
         cmd.append(self.config["pcap"])
-        self.tcpdump = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        print(cmd)
+        self.tcpdump = subprocess.Popen(cmd) # , stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
     def __exit__(self, _, exc_value, __):
         if exc_value is not None or self.config.get('noclean'):
