1ba367c34bba5305c9a15b35d7753dd835412760
Štěpán Balážik <stepan.balazik@nic.cz>
2020-04-30 18:33:44 +0200

    remove bundled libfaketime and use faketime from distro

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -o errexit -o nounset
-
 MAKEDIR="$(dirname "$0")"
-# build env.sh only if needed
-# it is handy if someone is executing run.sh in parallel
-test ! -f "${MAKEDIR}/env.sh" && make depend -C "${MAKEDIR}"
-source "${MAKEDIR}/env.sh"
-
-python3 -m pytest -c "${MAKEDIR}/deckard_pytest.ini" --tb=short -q ${VERBOSE:+"--log-level=DEBUG"} "${MAKEDIR}" ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} --boxed "$@"
+faketime -m --exclude-monotonic "" python3 -m pytest -c "${MAKEDIR}/deckard_pytest.ini" --tb=short -q ${VERBOSE:+"--log-level=DEBUG"} "${MAKEDIR}" ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} --boxed "$@"
