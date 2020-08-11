65a39c1ef6efec5b01e101a3ff10b2c71d86e49c
Štěpán Balážik <stepan.balazik@nic.cz>
2020-08-11 11:18:06 +0200

    allow monotonic time to be faked optionally
    this is done by appending `-monotonic` to the .rpl filename
    e.g. `val_xyz-monotonic.rpl`

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -o errexit -o nounset
 MAKEDIR="$(dirname "$0")"
-faketime -m --exclude-monotonic "" python3 -m pytest -c "${MAKEDIR}/deckard_pytest.ini" --tb=short -q ${VERBOSE:+"--log-level=DEBUG"} "${MAKEDIR}" ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} --boxed "$@"
+faketime -m --exclude-monotonic "" python3 -m pytest -c "${MAKEDIR}/deckard_pytest.ini" --tb=short -q ${VERBOSE:+"--log-level=DEBUG"} "${MAKEDIR}" ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} -m "not monotonic" --boxed "$@"
+faketime -m "" python3 -m pytest -c "${MAKEDIR}/deckard_pytest.ini" --tb=short -q ${VERBOSE:+"--log-level=DEBUG"} "${MAKEDIR}" ${DECKARDFLAGS:-} ${TESTS:+"--scenarios=${TESTS}"} -m "monotonic" --boxed "$@"
+exit $(( $? == 5 ? 0 : $? ))  # Currently there are no tests requiring faking monotonic time in this repository (there might be some in future)
+# pytest returns code 5 on "no tests where run" so we just ignore it
