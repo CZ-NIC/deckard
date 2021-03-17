79acd86c50c1ba8c78105017a119b86dfc97195c
Petr Špaček <pspacek@isc.org>
2021-03-17 16:09:37 +0100

    support for BIND
    Older versions of BIND require commenting out qname-minimization configuration.
    
    BIND builds linked to jemalloc or Clang ASAN will fail to start
    on systems with libfaketime versions > 0.9.6:
    - jemalloc https://github.com/wolfcw/libfaketime/issues/130
    - Clang ASAN https://github.com/wolfcw/libfaketime/issues/365

---
+++ named_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit -o nounset
+named -V | grep --quiet -- '--without-jemalloc' || echo 'WARNING: Make sure BIND is compiled without jemalloc library; for 9.17+ use ./configure --without-jemalloc'
+MINOR="$(named -v | cut -d . -f 2)"
+if [[ "$MINOR" -le "13" ]]
+then
+	echo 'WARNING: For BIND <= 9.13.2 manually remove qname-minimization option from named.conf template referenced in configs/named.yaml (usually template/named.j2)'
+fi
+
+RUNDIR="$(dirname "$0")"
+cd "$RUNDIR" && ./run.sh --config configs/named.yaml "$@"
